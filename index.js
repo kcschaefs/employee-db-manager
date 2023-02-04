ecdb62c08e1d9ecae3ba2f00b95ba2acb7cf2a8c
Kristen Schaefer <kristencschaefer@gmail.com>
2023-02-04 15:25:07 -0600

    added add department functionality

--- index.js
+++ index.js
@@ -18,29 +18,59 @@ const options = [
     type: 'list',
     name: 'startOptions',
     message: "What would you like to do?",
-    choices: ["View all Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
+    choices: ["View all Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Quit"]
   }
 ];
 
-function askUser(){
-  inquirer.prompt(options).then(optionsAnswers=>{
-    if(optionsAnswers.startOptions==="View all Departments"){
+const addDeptQuestions = [
+  {
+    type: 'input',
+    name: 'dept_name',
+    message: "Name of Department",
+  }
+]
+
+function addDept() {
+  inquirer.prompt(addDeptQuestions).then(deptAnswers => {
+    let deptAdd = deptAnswers.dept_name;
+    db.query(`INSERT INTO department (dept_name) VALUES (?)`, [deptAdd], function (err, results) {
+      if (err){
+        console.log(err);
+      } else console.log("Department Added!");
+      askUser();
+    })
+  });
+  };
+
+function askUser() {
+  inquirer.prompt(options).then(optionsAnswers => {
+    if (optionsAnswers.startOptions === "View all Departments") {
       db.query("SELECT dept_name AS 'dept name' FROM department;", function (err, results) {
         console.table(results);
         askUser();
       })
-      } else if(optionsAnswers.startOptions==="View All Roles"){
-        db.query("SELECT role.title AS title, role.salary AS salary, department.dept_name AS department FROM role JOIN department ON role.department_id=department.id;", function (err, results) {
-          console.table(results);
-          askUser();
-        })
-      } else if(optionsAnswers.startOptions==="View All Employees"){
-        db.query("SELECT employee.first_name AS 'first name', employee.last_name AS 'last name', role.title AS title FROM employee JOIN role ON employee.role_id=role.id;", function (err, results) {
-          console.table(results);
-          askUser();
-        })
-      }
-    });
-  }
+    } else if (optionsAnswers.startOptions === "View All Roles") {
+      db.query("SELECT role.title AS title, role.salary AS salary, department.dept_name AS department FROM role JOIN department ON role.department_id=department.id;", function (err, results) {
+        console.table(results);
+        askUser();
+      })
+    } else if (optionsAnswers.startOptions === "View All Employees") {
+      db.query("SELECT employee.first_name AS 'first name', employee.last_name AS 'last name', role.title AS title FROM employee JOIN role ON employee.role_id=role.id;", function (err, results) {
+        console.table(results);
+        askUser();
+      })
+    } else if (optionsAnswers.startOptions === "Add a Department") {
+        addDept();
+    } 
+    
+    
+    
+    
+    else {
+      db.end();
+    }
+  });
+}
 
-askUser();
+// askUser();
+
